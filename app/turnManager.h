28478c9a2133d1797946d78755c2d8c3952d1e84
franpog859 <franpog859@student.polsl.pl>
2018-04-24 12:01:40 +0200

    refactor of turn functions to turnManager

---
+++ app/turnManager.h
@@ -0,0 +1,40 @@
+#ifndef TURNMANAGER_H
+#define TURNMANAGER_H
+
+#include "gameBoard.h"
+#include "inputChecker.h"
+#include <stdbool.h>
+
+void doTheTurn(GameBoard* gameBoard);
+
+void changeActivePlayer(GameBoard* gameBoard);
+
+int getOponent(GameBoard* gameBoard);
+
+void getMove(GameBoard* gameBoard, Location* moveLocation);
+
+void setMove(GameBoard* gameBoard, Location* moveLocation);
+
+void setMissingMove(GameBoard* gameBoard, Location* location);
+
+void setKillingMove(GameBoard* gameBoard, Location* location);
+
+bool isEntirelyKilled(GameBoard* gameBoard, Location* actualLocation, Location* previousLocation);
+
+void initializeProbeLocation(Location* probeLocation, int col, int row);
+
+bool checkForProbeLocation(GameBoard* gameBoard, bool isEntirelyKilledTemp,
+	Location* probeLocation, Location* actualLocation, Location* previousLocation);
+
+bool isThereShip(GameBoard* gameBoard, Location* location);
+
+bool isLocationKilled(GameBoard* gameBoard, Location* probeLocation);
+
+void killEntireShip(GameBoard* gameBoard, Location* location);
+
+bool isNotKilledShip(GameBoard* gameBoard, Location* probeLocation);
+
+void increasePlayersPoints(GameBoard* gameBoard);
+
+#endif
+
