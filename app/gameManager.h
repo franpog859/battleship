fe126a3fc016e7a580c030b5a747347c263626e7
franpog859 <franpog859@student.polsl.pl>
2018-04-24 12:01:28 +0200

    refactor of turn functions to turnManager

--- app/gameManager.h
+++ app/gameManager.h
@@ -12,37 +12,6 @@ void prepareGameBoard(GameBoard* gameBoard, Parameters* params);
 
 bool isGameOn(GameBoard* gameBoard);
 
-void doTheTurn(GameBoard* gameBoard);
-
-void changeActivePlayer(GameBoard* gameBoard);
-
-int getOponent(GameBoard* gameBoard);
-
-void getMove(GameBoard* gameBoard, Location* moveLocation);
-
-void setMove(GameBoard* gameBoard, Location* moveLocation);
-
-void setMissingMove(GameBoard* gameBoard, Location* location);
-
-void setKillingMove(GameBoard* gameBoard, Location* location);
-
-bool isEntirelyKilled(GameBoard* gameBoard, Location* actualLocation, Location* previousLocation);
-
-void initializeProbeLocation(Location* probeLocation, int col, int row);
-
-bool checkForProbeLocation(GameBoard* gameBoard, bool isEntirelyKilledTemp,
-	Location* probeLocation, Location* actualLocation, Location* previousLocation);
-
-bool isThereShip(GameBoard* gameBoard, Location* location);
-
-bool isLocationKilled(GameBoard* gameBoard, Location* probeLocation);
-
-void killEntireShip(GameBoard* gameBoard, Location* location);
-
-bool isNotKilledShip(GameBoard* gameBoard, Location* probeLocation);
-
-void increasePlayersPoints(GameBoard* gameBoard);
-
 void incrementNumberOfTurns(GameBoard* gameBoard);
 
 void publicWinner(GameBoard* gameBoard);
